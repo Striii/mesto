51c9d648aa40be9b412c403d8e327c694bb141ea
Alex Stepanenko <giprozem-kurs@yandex.ru>
2022-04-17 21:53:35 +0300

    add function add one card

--- script/index.js
+++ script/index.js
@@ -11,8 +11,7 @@ const popupUserHobby = document.querySelector('.profile__hobby');
 const nameInput = document.querySelector('.popup__input_field_name');
 const nameHobby = document.querySelector('.popup__input_field_hobby');
 const myForm = document.querySelector('.popup__form');
-const fieldMesto = document.querySelector('.popup__input_field_mesto');
-const fieldImg = document.querySelector('.popup__input_field_img');
+
 
 
 
@@ -46,16 +45,10 @@ function inputPopupUserName(event) {
     event.preventDefault();
     popupUserName.textContent = nameInput.value;
     popupUserHobby.textContent = nameHobby.value;
-    closeModalWindow(modalWindowName)
+    closeModalWindow(modalWindowName);
 }
 
 myForm.addEventListener('submit', inputPopupUserName);
-
-
-
-
-
-
 
 
 
@@ -84,59 +77,34 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
 const listContainer = document.querySelector('.grid__wrap');
 const template = document.querySelector('.template-card');
+const myFormAddCard = document.querySelector('.popup__form_card');
+const inputNameMesto = document.querySelector('.popup__input_field_mesto');
+const inputFiledImg = document.querySelector('.popup__input_field_img');
 
-
-function render() {
+function renderCard() {
     const html = initialCards.map(getElement);
     listContainer.append(...html);
 }
 
 function getElement(item) {
     const getElementTemplate = template.content.cloneNode(true);
-    const title = getElementTemplate.querySelector('.card__title');
-    const cardImages = getElementTemplate.querySelector('.card__img');
-    console.log(item.link);
-    cardImages.src = item.link;
-    cardImages.alt = item.name;
-    title.textContent = item.name;
-
+    const name = getElementTemplate.querySelector('.card__title');
+    const link = getElementTemplate.querySelector('.card__img');
+    name.textContent = item.name;
+    link.src = item.link;
     return getElementTemplate;
 }
 
-render();
+renderCard();
 
-const ActionAddBtn = document.querySelector('.profile__btn-add');
-
-
-function inputPopupAddCard() {
-    // cardImages.src = fieldImg.value;
-    // cardImages.alt = fieldMesto.value;
-    // title.textContent = fieldMesto.value;
-    modalWindow.classList.add('popup__opened_add');
+function handleAddCard(e) {
+    e.preventDefault();
+    listContainer.prepend(getElement(({ name: inputNameMesto.value, link: inputFiledImg.value })));
+    closeModalWindow(modalWindowAdd);
+    myFormAddCard.reset();
 }
 
-ActionAddBtn.addEventListener('click', inputPopupAddCard);
-
-
-// function openModalWindow() {
-//   nameInput.value = popupUserName.textContent;
-//   nameHobby.value = popupUserHobby.textContent;
-//   modalWindow.classList.add('popup_opened');
-// }
-
-// function closeModalWindow() {
-//   modalWindow.classList.remove('popup_opened');
-// }
-
-// function inputPopupUserName(event) {
-//   event.preventDefault();
-//   popupUserName.textContent = nameInput.value;
-//   popupUserHobby.textContent = nameHobby.value;
-//   closeModalWindow()
-// }
-
-// ActionEditBtn.addEventListener('click', openModalWindow);
-// ModalCloseBtn.addEventListener('click', closeModalWindow);
-// myForm.addEventListener('submit', inputPopupUserName);
+myFormAddCard.addEventListener('submit', handleAddCard);
